0cc711504101768a308629928366e351ef120fe2
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-18 09:01:50 -0300

    Classe MovieControllerRa feita

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,84 +1,135 @@
 package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class MovieControllerRA {
 
-	private Long existsMovieId, nonExistsMovieId;
-	private String clientToken, adminToken, invalidToken;
-	private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private Long existsMovieId, nonExistsMovieId;
+    private String clientToken, adminToken, invalidToken;
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
 
-	@BeforeEach
-	public void setup() throws JSONException {
-		baseURI = "http://localhost:8080";
+    private Map<String, Object> postMovieInstance;
 
-		existsMovieId = 1L;
-		nonExistsMovieId = 999L;
+    @BeforeEach
+    public void setup() throws JSONException {
+        baseURI = "http://localhost:8080";
 
-		clientUsername = "joaquim@gmail.com";
-		clientPassword = "123456";
+        existsMovieId = 1L;
+        nonExistsMovieId = 999L;
 
-		adminUsername = "maria@gmail.com";
-		adminPassword = "123456";
+        clientUsername = "joaquim@gmail.com";
+        clientPassword = "123456";
 
-		clientToken = TokenUtil.obtainAccessToken(clientUsername,clientPassword);
-		adminToken = TokenUtil.obtainAccessToken(adminUsername,adminPassword);
-		invalidToken = adminToken + "xpto";
-	}
-	
-	@Test
-	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto";
+
+        postMovieInstance = new HashMap<>();
+        postMovieInstance.put("title", "Test Movie");
+        postMovieInstance.put("score", 0.0);
+        postMovieInstance.put("count", 0);
+        postMovieInstance.put("image", "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
+    }
+
+    @Test
+    public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+
+        given()
+                .when()
+                .get("/movies")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+        given()
+                .queryParam("title", "Matrix")
+                .when()
+                .get("/movies")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void findByIdShouldReturnMovieWhenIdExists() {
+        given()
+                .when()
+                .get("/movies/{id}", existsMovieId)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+        given()
+                .when()
+                .get("/movies/{id}", nonExistsMovieId)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+		postMovieInstance.put("title", "   ");
+        JSONObject newMovieInstance = new JSONObject(postMovieInstance);
+
+        given()
+                .header("Content-Type", "application/json ")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newMovieInstance.toJSONString())
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/movies")
+                .then()
+                .statusCode(422);
+    }
+
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+		JSONObject newMovieInstance = new JSONObject(postMovieInstance);
 
 		given()
+				.header("Content-Type", "application/json ")
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newMovieInstance.toJSONString())
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
 				.when()
-				.get("/movies")
+				.post("/movies")
 				.then()
-				.statusCode(200);
-	}
-	
-	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+				.statusCode(403);
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+		JSONObject newMovieInstance = new JSONObject(postMovieInstance);
+
 		given()
-				.queryParam("title", "Matrix")
+				.header("Content-Type", "application/json ")
+				.header("Authorization", "Bearer " + invalidToken)
+				.body(newMovieInstance.toJSONString())
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
 				.when()
-				.get("/movies")
+				.post("/movies")
 				.then()
-				.statusCode(200);
-	}
-	
-	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {
-		given()
-				.when()
-				.get("/movies/{id}", existsMovieId)
-				.then()
-				.statusCode(200);
-	}
-	
-	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
-		given()
-				.when()
-				.get("/movies/{id}", nonExistsMovieId)
-				.then()
-				.statusCode(404);
-	}
-	
-	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
-	}
-	
-	@Test
-	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
-	}
-	
-	@Test
-	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
-	}
+				.statusCode(401);
+    }
 }
